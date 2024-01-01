8a168de88909df73d1e730670914f94bfa3a82ca
Amar Kumar Ram <ram.amarkumar@gmail.com>
2024-01-01 10:28:23 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
     const pathname = decodeURIComponent(location.href);
     let name = pathname.substring(pathname.indexOf('=') + 1);
-    if(name !== "") {
+    if(name !== "" && pathname.indexOf("=") !== -1) {
         document.getElementById("msgFromName").innerHTML = name;
     }
 })
